dec624a813707f838025a1da4d8d3a91165fc658
Kaitlin Kelly <kk504408@gmail.com>
2018-07-09 11:45:33 +0000

    automatically backed up by learn

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -3,10 +3,10 @@ class Classification < ActiveRecord::Base
   has_many :boats, through: :boat_classifications
 
   def my_all
-    
+    select(:id, :name, :admiral, :created_at, :updated_at).uniq
   end
 
   def longest
   end
-  
+
 end
