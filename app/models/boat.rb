109247c1a393e5ca64ad4e3203e9a9082a1da32e
Kaitlin Kelly <kk504408@gmail.com>
2018-07-09 11:51:00 +0000

    automatically backed up by learn

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -4,7 +4,8 @@ class Boat < ActiveRecord::Base
   has_many    :classifications, through: :boat_classifications
 
   def self.first_five
-    order(id: :asc).limit(5)
+    #order(id: :asc).limit(5)
+    all.limit(5)
   end
 
   def self.dinghy
